4f66ad185f10f84b42c4ea1b990e8e3c470382e4
marcelonespoli <marcelonespoli@outlook.com>
2020-01-09 15:49:54 +0000

    Add files

---
+++ GlobalExceptionHandlingFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using MyProject.Business.Models;
+using System.Net;
+
+namespace Scheduler.Api.GlobalFilters
+{
+    public class GlobalExceptionHandlingFilter : IExceptionFilter
+    {
+        private readonly ILogger<GlobalExceptionHandlingFilter> _logger;
+
+        public GlobalExceptionHandlingFilter(ILogger<GlobalExceptionHandlingFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError($"API: {context.Exception.Message}", context.Exception);
+
+            //_logger.LogError(new EventId(context.Exception.HResult), context.Exception, $"API: {context.Exception.Message}");
+
+            var errorDetails = new Notification
+            {
+                Statuscode = HttpStatusCode.InternalServerError,
+                Success = false,
+                Error = "An unexpected error ocurred"
+            };
+            
+            context.ExceptionHandled = true;
+            context.Result = new ObjectResult(errorDetails)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Enterprise.Common.Authentication;
+using Enterprise.Common.Authentication.Extensions;
+using Enterprise.Common.Authentication.Services.BearerToken;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Serilog;
+using Serilog.Context;
+using Swashbuckle.AspNetCore.Swagger;
+using UAParser;
+using SameSiteMode = Microsoft.AspNetCore.Http.SameSiteMode;
+using Microsoft.OpenApi.Models;
+using MyProject.Api.GlobalFilters;
+using Microsoft.Extensions.Logging;
+
+namespace MyProject.Api
+{
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;			
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add MVC
+			services.AddMvc(options =>
+			{
+				options.Filters.Add(new ProducesAttribute("application/json"));
+				options.Filters.Add(typeof(GlobalExceptionHandlingFilter));
+			})
+			.SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+
+			services.AddScoped<ILogger<GlobalExceptionHandlingFilter>, Logger<GlobalExceptionHandlingFilter>>();
+			services.AddScoped<GlobalExceptionHandlingFilter>();
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			
+			app.MapWhen(x => !x.Request.Path.Value.StartsWith("/api/"), builder =>
+			{
+				builder.UseSpa(spa =>
+				{
+					spa.Options.SourcePath = "app";
+				});
+			});
+
+			app.UseMvc();
+		}
+
+
+	}
+}
